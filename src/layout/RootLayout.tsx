5919bc564b7dc978c7b2e35b90cbd08aaf658105
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2023-10-11 19:35:57 +0200

    fix deploy bug

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet, ScrollRestoration } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import AsideMenu from "./AsideMenu/AsideMenu";
-import { SideMenuProvider } from "../context/sideMenuContext";
+import { SideMenuProvider } from "../context/SideMenuContext";
 
 export function RootLayout() {
   return (
