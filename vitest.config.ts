4f8a39c1ad5140d7e20a349cfa4c947e08b8fb86
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-01-12 10:13:12 +0100

    Added coverage option to vitest test

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,5 +7,22 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: ["./src/setup-test/setupTests.ts"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+      exclude: [
+        "**/*.d.ts",
+        "**/types/**",
+        "**/constants/**",
+        "src/Icons/**",
+        "src/main.tsx",
+      ],
+      thresholds: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: -10,
+      },
+    },
   },
 });
