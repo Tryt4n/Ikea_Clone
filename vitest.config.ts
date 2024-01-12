4abc368053b02dfd26059e93681adbe0da9df7a5
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-01-12 16:24:27 +0100

    Excluded `setup-test` directory from tests coverage.

--- vitest.config.ts
+++ vitest.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
         "**/constants/**",
         "src/Icons/**",
         "src/main.tsx",
+        "src/setup-test/**",
       ],
       thresholds: {
         branches: 80,
