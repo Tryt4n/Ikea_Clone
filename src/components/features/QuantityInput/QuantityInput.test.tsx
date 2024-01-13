7f168741d86254bc94a1081ed8215bd83cc2b680
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-01-13 10:57:19 +0100

    Added tests.

---
+++ src/components/features/QuantityInput/QuantityInput.test.tsx
@@ -0,0 +1,225 @@
+import { describe, it, expect, vi } from "vitest";
+import { screen } from "@testing-library/react";
+import { render } from "../../../setup-test/test-utils";
+import userEvent from "@testing-library/user-event";
+import QuantityInput from "./QuantityInput";
+
+describe("QuantityInput", () => {
+  it('should render an element with class "quantity-input" and the correct components', () => {
+    // Arrange
+    const quantity = 1;
+    const inputFunction = vi.fn();
+    const onChangeFunction = vi.fn();
+
+    // Act
+    render(
+      <QuantityInput
+        quantity={quantity}
+        inputFunction={inputFunction}
+        onChangeFunction={onChangeFunction}
+      />
+    );
+    const container = screen.getByTestId("quantity-input");
+    const minusBtn = screen.getByRole("button", {
+      name: "Naciśnij aby zmniejszyć ilość",
+    });
+    const plusBtn = screen.getByRole("button", {
+      name: "Naciśnij aby zwiększyć ilość",
+    });
+    const textInput = screen.getByRole("textbox", {
+      name: "Wpisz liczbę lub zmień jej wartość za pomocą znaku plus i minus.",
+    });
+
+    // Assert
+    expect(container).toBeInTheDocument();
+    expect(minusBtn).toBeInTheDocument();
+    expect(plusBtn).toBeInTheDocument();
+    expect(textInput).toBeInTheDocument();
+  });
+
+  it("should render a container with additional classes if they are provided or it is small variant", () => {
+    // Arrange
+    const quantity = 1;
+    const inputFunction = vi.fn();
+    const onChangeFunction = vi.fn();
+    const additionalClasses = "additional-class";
+
+    // Act
+    render(
+      <QuantityInput
+        quantity={quantity}
+        inputFunction={inputFunction}
+        onChangeFunction={onChangeFunction}
+        className={additionalClasses}
+        small={true}
+      />
+    );
+    const container = screen.getByTestId("quantity-input");
+
+    // Assert
+    expect(container).toHaveClass(additionalClasses);
+    expect(container).toHaveClass("quantity-input--small");
+  });
+
+  it("should disable the Btn component for decreasing the quantity when the quantity is 1", () => {
+    // Arrange
+    const quantity = 1;
+    const inputFunction = vi.fn();
+    const onChangeFunction = vi.fn();
+
+    // Act
+    render(
+      <QuantityInput
+        quantity={quantity}
+        inputFunction={inputFunction}
+        onChangeFunction={onChangeFunction}
+      />
+    );
+
+    // Assert
+    expect(
+      screen.getByRole("button", { name: "Naciśnij aby zmniejszyć ilość" })
+    ).toBeDisabled();
+  });
+
+  it("should disable the Btn component for increasing the quantity when the quantity is 99", () => {
+    // Arrange
+    const quantity = 99;
+    const inputFunction = vi.fn();
+    const onChangeFunction = vi.fn();
+
+    // Act
+    render(
+      <QuantityInput
+        quantity={quantity}
+        inputFunction={inputFunction}
+        onChangeFunction={onChangeFunction}
+      />
+    );
+
+    // Assert
+    expect(
+      screen.getByRole("button", { name: "Naciśnij aby zwiększyć ilość" })
+    ).toBeDisabled();
+  });
+
+  it("should call the inputFunction when the input value changes", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    const quantity = 1;
+    const inputFunction = vi.fn();
+    const onChangeFunction = vi.fn();
+
+    // Act
+    render(
+      <QuantityInput
+        quantity={quantity}
+        inputFunction={inputFunction}
+        onChangeFunction={onChangeFunction}
+      />
+    );
+    const textInput = screen.getByRole("textbox", {
+      name: "Wpisz liczbę lub zmień jej wartość za pomocą znaku plus i minus.",
+    });
+    await user.type(textInput, "2");
+
+    // Assert
+    expect(inputFunction).toHaveBeenCalled();
+  });
+
+  it("should call the onChangeFunction with 1 when the plus Btn is clicked", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    const quantity = 1;
+    const inputFunction = vi.fn();
+    const onChangeFunction = vi.fn();
+
+    // Act
+    render(
+      <QuantityInput
+        quantity={quantity}
+        inputFunction={inputFunction}
+        onChangeFunction={onChangeFunction}
+      />
+    );
+    const plusBtn = screen.getByRole("button", {
+      name: "Naciśnij aby zwiększyć ilość",
+    });
+    await user.click(plusBtn);
+
+    // Assert
+    expect(onChangeFunction).toHaveBeenCalledWith(1);
+  });
+
+  it("should call the onChangeFunction with -1 when the minus Btn is clicked and quantity is bigger than 1", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    const quantity = 2;
+    const inputFunction = vi.fn();
+    const onChangeFunction = vi.fn();
+
+    // Act
+    render(
+      <QuantityInput
+        quantity={quantity}
+        inputFunction={inputFunction}
+        onChangeFunction={onChangeFunction}
+      />
+    );
+    const minusBtn = screen.getByRole("button", {
+      name: "Naciśnij aby zmniejszyć ilość",
+    });
+    await user.click(minusBtn);
+
+    // Assert
+    expect(onChangeFunction).toHaveBeenCalledWith(-1);
+  });
+
+  it("should not call the onChangeFunction when the plus Btn is clicked and quantity is 99", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    const quantity = 99;
+    const inputFunction = vi.fn();
+    const onChangeFunction = vi.fn();
+
+    // Act
+    render(
+      <QuantityInput
+        quantity={quantity}
+        inputFunction={inputFunction}
+        onChangeFunction={onChangeFunction}
+      />
+    );
+    const plusBtn = screen.getByRole("button", {
+      name: "Naciśnij aby zwiększyć ilość",
+    });
+    await user.click(plusBtn);
+
+    // Assert
+    expect(onChangeFunction).not.toHaveBeenCalled();
+  });
+
+  it("should not call the onChangeFunction when the minus Btn is clicked and quantity is 1", async () => {
+    // Arrange
+    const user = userEvent.setup();
+    const quantity = 1;
+    const inputFunction = vi.fn();
+    const onChangeFunction = vi.fn();
+
+    // Act
+    render(
+      <QuantityInput
+        quantity={quantity}
+        inputFunction={inputFunction}
+        onChangeFunction={onChangeFunction}
+      />
+    );
+    const minusBtn = screen.getByRole("button", {
+      name: "Naciśnij aby zmniejszyć ilość",
+    });
+    await user.click(minusBtn);
+
+    // Assert
+    expect(onChangeFunction).not.toHaveBeenCalled();
+  });
+});
---
+++ src/components/features/RatingBlock/RatingBlock.test.tsx
@@ -0,0 +1,86 @@
+import { describe, it, expect } from "vitest";
+import { screen } from "@testing-library/react";
+import { render } from "../../../setup-test/test-utils";
+import userEvent from "@testing-library/user-event";
+import RatingBlock from "./RatingBlock";
+
+describe("RatingBlock", () => {
+  it("should render the rating score and number of ratings when given a valid rating object", () => {
+    // Arrange
+    const rating = {
+      rate: 4.5,
+      quantity: 10,
+    };
+
+    // Act
+    render(<RatingBlock rating={rating} />);
+
+    // Assert
+    expect(screen.getByText(`Ocena: ${rating.rate}`)).toBeInTheDocument();
+    expect(screen.getByText(`Liczba ocen:`)).toBeInTheDocument();
+    expect(screen.getByText(`${rating.quantity}`)).toBeInTheDocument();
+  });
+
+  it("should render the correct number of filled and empty stars based on the rating score", () => {
+    // Arrange
+    const rating = {
+      rate: 3.5,
+      quantity: 10,
+    };
+
+    // Act
+    render(<RatingBlock rating={rating} />);
+    const filledStars = screen.getAllByTestId("rating-star-icon");
+    const emptyStars = screen.getAllByTestId("rating-star-empty-icon");
+    const halfStar = screen.getByTestId("rating-star-half-icon");
+
+    // Assert
+    expect(filledStars.length).toBe(3);
+    expect(emptyStars.length).toBe(1);
+    expect(halfStar).toBeInTheDocument();
+  });
+
+  it("should render 5 empty stars if the rating score is 0", () => {
+    // Arrange
+    const rating = {
+      rate: 0,
+      quantity: 10,
+    };
+
+    // Act
+    render(<RatingBlock rating={rating} />);
+
+    // Assert
+    const emptyStars = screen.getAllByTestId("rating-star-empty-icon");
+    expect(emptyStars.length).toBe(5);
+  });
+
+  it("should render 5 filled stars if the rating score is 5", () => {
+    // Arrange
+    const rating = {
+      rate: 5,
+      quantity: 10,
+    };
+
+    // Act
+    render(<RatingBlock rating={rating} />);
+
+    // Assert
+    const filledStars = screen.getAllByTestId("rating-star-icon");
+    expect(filledStars.length).toBe(5);
+  });
+
+  it("should render long version of the rating when the longVersion prop is true", () => {
+    // Arrange
+    const rating = {
+      rate: 3.5,
+      quantity: 10,
+    };
+
+    // Act
+    render(<RatingBlock rating={rating} longVersion={true} />);
+
+    // Assert
+    expect(screen.getByText(`${rating.quantity} recenzji`)).toBeInTheDocument();
+  });
+});
