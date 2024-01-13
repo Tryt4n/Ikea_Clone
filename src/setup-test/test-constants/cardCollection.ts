4023497372dd365d96d759cebe4aa9afac37ed3a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-01-13 09:31:27 +0100

    Added constant for testing

---
+++ src/setup-test/test-constants/cardCollection.ts
@@ -0,0 +1,74 @@
+import type { CardCollectionType } from "../../layout/Articles/components/ImageCardCollection/ImageCardCollection";
+
+export const cardCollection: CardCollectionType[] = [
+  {
+    id: "1",
+    img: {
+      imgSrc: "image1.jpg",
+      imgSrcSet: "image1.jpg 1x",
+      imgAlt: "Image 1",
+      imgAspectRatio: "16/9",
+    },
+    products: [],
+  },
+  {
+    id: "2",
+    img: {
+      imgSrc: "image2.jpg",
+      imgSrcSet: "image2.jpg 1x",
+      imgAlt: "Image 2",
+      imgAspectRatio: "3/4",
+    },
+    products: [],
+  },
+  {
+    id: "3",
+    img: {
+      imgSrc: "image3.jpg",
+      imgSrcSet: "image3.jpg 1x",
+      imgAlt: "Image 3",
+      imgAspectRatio: "1/1",
+    },
+    products: [],
+  },
+  {
+    id: "4",
+    img: {
+      imgSrc: "image4.jpg",
+      imgSrcSet: "image4.jpg 1x",
+      imgAlt: "Image 4",
+      imgAspectRatio: "16/9",
+    },
+    products: [],
+  },
+  {
+    id: "5",
+    img: {
+      imgSrc: "image5.jpg",
+      imgSrcSet: "image5.jpg 1x",
+      imgAlt: "Image 5",
+      imgAspectRatio: "3/4",
+    },
+    products: [],
+  },
+  {
+    id: "6",
+    img: {
+      imgSrc: "image6.jpg",
+      imgSrcSet: "image6.jpg 1x",
+      imgAlt: "Image 6",
+      imgAspectRatio: "1/1",
+    },
+    products: [],
+  },
+  {
+    id: "7",
+    img: {
+      imgSrc: "image7.jpg",
+      imgSrcSet: "image7.jpg 1x",
+      imgAlt: "Image 7",
+      imgAspectRatio: "16/9",
+    },
+    products: [],
+  },
+];
