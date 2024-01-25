7bea2bfd8a1fa3a83bcc144df9295f5fa40bc205
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-01-25 10:26:03 +0100

    Added `rendersWithRoutes` function.

--- src/setup-test/test-utils.ts
+++ src/setup-test/test-utils.ts
@@ -1,15 +1,21 @@
 import type { ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 // Context providers
-import { AllTheProviders } from "./AllTheProviders";
+import { AllTheProviders, AllTheProvidersWithRoutes } from "./AllTheProviders";
 
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "wrapper">,
 ) => render(ui, { wrapper: AllTheProviders, ...options });
 
+// Render with routes because <ScrollRestoration /> component from `react-router-dom` cannot be used outside a <Router /> component so wrapping in <MemoryRouter> would not work
+const renderWithRoutes = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">,
+) => render(ui, { wrapper: AllTheProvidersWithRoutes, ...options });
+
 export * from "@testing-library/react";
-export { customRender as render };
+export { customRender as render, renderWithRoutes };
 
 export function areDatesEqual(date1: Date, date2: Date): boolean {
   return date1.toISOString().slice(0, 19) === date2.toISOString().slice(0, 19);
