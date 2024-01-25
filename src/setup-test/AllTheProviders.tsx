9a042cf3c1306678a918fee90e3e56565b8ae9f5
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-01-25 10:25:29 +0100

    Added provider with routes.

--- src/setup-test/AllTheProviders.tsx
+++ src/setup-test/AllTheProviders.tsx
@@ -1,22 +1,33 @@
-import React from "react";
+import { ReactNode } from "react";
+// Router dependencies
+import { MemoryRouter, RouterProvider } from "react-router-dom";
 // Context providers
 import { AppContextProvider } from "../context/AppContext/AppContext";
 import { ToastContextProvider } from "../context/ToastContext/ToastContext";
 import { ModalContextProvider } from "../context/ModalContext/ModalContext";
-import { MemoryRouter } from "react-router-dom";
+// routes
+import { routes } from "../routes";
 
-export const AllTheProviders = ({
-  children,
-}: {
-  children: React.ReactNode;
-}) => {
+export const AllTheProviders = ({ children }: { children: ReactNode }) => {
   return (
-    <AppContextProvider>
-      <ToastContextProvider>
-        <ModalContextProvider>
-          <MemoryRouter>{children}</MemoryRouter>
-        </ModalContextProvider>
-      </ToastContextProvider>
-    </AppContextProvider>
+    <ContextProviders>
+      <MemoryRouter>{children}</MemoryRouter>
+    </ContextProviders>
   );
 };
+
+export const AllTheProvidersWithRoutes = () => {
+  return (
+    <ContextProviders>
+      <RouterProvider router={routes} />
+    </ContextProviders>
+  );
+};
+
+const ContextProviders = ({ children }: { children: ReactNode }) => (
+  <AppContextProvider>
+    <ToastContextProvider>
+      <ModalContextProvider>{children}</ModalContextProvider>
+    </ToastContextProvider>
+  </AppContextProvider>
+);
