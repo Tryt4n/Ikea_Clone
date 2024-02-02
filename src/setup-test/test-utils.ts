36dd964b4a01027fb19a164172c009d129bbbb20
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-02 12:41:07 +0100

    Added new function for checking dates and deleted old one.

--- src/setup-test/test-utils.ts
+++ src/setup-test/test-utils.ts
@@ -17,6 +17,15 @@ const renderWithRoutes = (
 export * from "@testing-library/react";
 export { customRender as render, renderWithRoutes };
 
-export function areDatesEqual(date1: Date, date2: Date): boolean {
-  return date1.toISOString().slice(0, 19) === date2.toISOString().slice(0, 19);
+/**
+ * A helper function for testing dates. It compares two dates and checks if they are within a five-second interval of each other.
+ * This function is useful because during testing, the times can slightly differ depending on the speed of the tests execution.
+ *
+ * @param {Date} date1 - The first date to compare.
+ * @param {Date} date2 - The second date to compare.
+ * @returns {boolean} - Returns true if the dates are within five seconds of each other, otherwise returns false.
+ */
+export function isSimilarDate(date1: Date, date2: Date): boolean {
+  const diff = Math.abs(date1.getTime() - date2.getTime());
+  return diff <= 5000; // 5 seconds
 }
