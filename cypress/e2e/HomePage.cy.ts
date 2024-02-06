8540a127b8310227080792acd5bb41543a2a292a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-06 10:34:40 +0100

    Added tests.

---
+++ cypress/e2e/HomePage.cy.ts
@@ -0,0 +1,42 @@
+describe("Home Page", () => {
+  it("should show additional informations of products in main article", () => {
+    cy.get("[data-testid=collection-list]").first().as("collectionList");
+
+    cy.fixture("homePage.json").then((fixture) => {
+      cy.get("@collectionList")
+        .children()
+        .should("have.length", fixture.articles[0].content.products.length);
+
+      cy.get("@collectionList")
+        .children()
+        .each(($child) => {
+          cy.wrap($child).trigger("mouseover");
+          cy.wrap($child).within(() => {
+            cy.get("[role=tooltip]").should("be.visible");
+            cy.get("[role=tooltip]").should(
+              "have.attr",
+              "aria-hidden",
+              "false",
+            );
+          });
+        });
+
+      cy.get("@collectionList").children().first().trigger("mouseover");
+      cy.get("@collectionList")
+        .children()
+        .first()
+        .within(() => {
+          cy.get("[role=tooltip]").click();
+        });
+
+      cy.url().should((url) => {
+        expect(url).to.eq(
+          Cypress.config().baseUrl +
+            fixture.articles[0].content.products[0].productLink,
+        );
+      });
+
+      cy.go("back");
+    });
+  });
+});
