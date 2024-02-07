5cf1000676d965c20ce7c245294a58c05ffcdd18
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-07 13:07:00 +0100

    Added tests.

---
+++ cypress/e2e/Footer/Footer.cy.ts
@@ -0,0 +1,79 @@
+import { footerLists } from "../../../src/constants/footerLists";
+
+describe("Footer", () => {
+  it("should properly handle accordions on mobile view", () => {
+    cy.viewport(899, 1400);
+
+    cy.get("[data-testid=footer-accordions]").as("accordionsContainer");
+
+    cy.get("@accordionsContainer")
+      .children()
+      .should("have.length", footerLists.length);
+
+    cy.get("@accordionsContainer").within(() => {
+      cy.get("[role=region]").as("accordionsContent");
+    });
+
+    cy.get("@accordionsContent").each((accordion) => {
+      cy.wrap(accordion).should("not.be.visible");
+    });
+
+    cy.get("@accordionsContainer").children().first().as("firstAccordion");
+    cy.get("@accordionsContainer").children().eq(1).as("secondAccordion");
+
+    cy.get("@firstAccordion").within(() => {
+      cy.get("button").as("firstAccordionButton");
+      cy.get("[role=region]").as("firstAccordionContent");
+
+      cy.get("@firstAccordionButton").should(
+        "have.attr",
+        "aria-expanded",
+        "false",
+      );
+
+      // Open the first accordion
+      cy.get("@firstAccordionButton").click();
+      cy.get("@firstAccordionButton").should(
+        "have.attr",
+        "aria-expanded",
+        "true",
+      );
+      cy.get("@firstAccordionContent").should("be.visible");
+      cy.get("@accordionsContent").each((accordion, index) => {
+        if (index !== 0) {
+          cy.wrap(accordion).should("not.be.visible");
+        }
+      });
+    });
+
+    cy.get("@secondAccordion").within(() => {
+      cy.get("button").as("secondAccordionButton");
+      cy.get("[role=region]").as("secondAccordionContent");
+
+      // Open the second accordion
+      cy.get("@secondAccordionButton").click();
+      cy.get("@secondAccordionButton").should(
+        "have.attr",
+        "aria-expanded",
+        "true",
+      );
+      cy.get("@secondAccordionButton").should("be.visible");
+      cy.get("@accordionsContent").each((accordion, index) => {
+        if (index !== 1) {
+          cy.wrap(accordion).should("not.be.visible");
+        }
+      });
+
+      // Close the second accordion
+      cy.get("@secondAccordionButton").click();
+      cy.get("@secondAccordionButton").should(
+        "have.attr",
+        "aria-expanded",
+        "false",
+      );
+    });
+    cy.get("@accordionsContent").each((accordion) => {
+      cy.wrap(accordion).should("not.be.visible");
+    });
+  });
+});
--- cypress/e2e/HomePage/HomePage.cy.ts
+++ cypress/e2e/HomePage/HomePage.cy.ts
@@ -91,4 +91,61 @@ describe("Home Page", () => {
       cy.get("@nextButton").should("have.attr", "tabindex", "0");
     });
   });
+
+  it("should scroll the page on scroll to top button click", () => {
+    cy.get("[data-testid=back-to-top-btn]").as("backToTopButton");
+
+    cy.get("@backToTopButton").should("exist");
+    cy.get("@backToTopButton").should("not.be.visible");
+
+    // Scroll page
+    cy.window().then((win) => {
+      win.scrollBy(0, win.innerHeight);
+    });
+
+    // Check if button is visible
+    cy.get("@backToTopButton").should("be.visible");
+
+    // Click button
+    cy.get("@backToTopButton").click();
+    cy.window().its("scrollY").should("eq", 0);
+    cy.get("@backToTopButton").should("not.be.visible");
+  });
+
+  it("should handle images gallery behavior", () => {
+    cy.get("[data-testid=show-more-images-in-gallery-btn]").as(
+      "showMoreImagesButton",
+    );
+    cy.get("[data-testid=images-gallery-container]").as("gallery");
+
+    cy.get("@showMoreImagesButton")
+      .invoke("text")
+      .then((text) => {
+        const match = text.match(/\d+/); // Find a number in the text
+        if (match) {
+          const expectedChildrenCount = parseInt(match[0], 10); // Convert the found number to an integer
+
+          cy.get("@gallery")
+            .children()
+            .then((children) => {
+              let initialChildrenCount = children.length;
+
+              cy.get("@gallery")
+                .children()
+                .should("have.length", expectedChildrenCount);
+
+              cy.get("@showMoreImagesButton").click();
+
+              cy.get("@gallery")
+                .children()
+                .should(
+                  "have.length",
+                  initialChildrenCount + expectedChildrenCount,
+                );
+              initialChildrenCount =
+                initialChildrenCount + expectedChildrenCount;
+            });
+        }
+      });
+  });
 });
