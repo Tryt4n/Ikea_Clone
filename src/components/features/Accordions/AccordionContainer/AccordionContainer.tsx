5455b16aaad0665b3aba336adf1e10fd45a0ff82
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-07 13:08:24 +0100

    Added all `ul` element props to component.

--- src/components/features/Accordions/AccordionContainer/AccordionContainer.tsx
+++ src/components/features/Accordions/AccordionContainer/AccordionContainer.tsx
@@ -1,5 +1,5 @@
 // Import React dependencies
-import { ReactElement } from "react";
+import { ReactElement, type HTMLProps } from "react";
 // Import context
 import { AccordionContextProvider } from "../context/AccordionContext";
 // Import components
@@ -8,11 +8,11 @@ import AccordionElement from "../AccordionElement/AccordionElement";
 import "../index.scss";
 
 // Define the type for the AccordionContainer props
-export interface AccordionContainerPropsType {
+export type AccordionContainerPropsType = {
   children:
     | ReactElement<typeof AccordionElement>[]
     | ReactElement<typeof AccordionElement>; // The children of the AccordionContainer, which should be one or more AccordionElement components
-}
+} & HTMLProps<HTMLUListElement>; // The props for the ul element
 
 /**
  * AccordionContainer component
@@ -26,10 +26,13 @@ export interface AccordionContainerPropsType {
 
 export default function AccordionContainer({
   children,
+  ...props
 }: AccordionContainerPropsType) {
   return (
     <AccordionContextProvider>
-      <ul className="accordions-container">{children}</ul>
+      <ul className="accordions-container" {...props}>
+        {children}
+      </ul>
     </AccordionContextProvider>
   );
 }
