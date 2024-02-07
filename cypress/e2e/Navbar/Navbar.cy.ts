3872017b2e9118f804ea9d401bba3c372ae47721
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-07 10:40:29 +0100

    Moved to its own directory.

---
+++ cypress/e2e/Navbar/Navbar.cy.ts
@@ -0,0 +1,22 @@
+describe("Navbar", () => {
+  it("should properly navigate through pages", () => {
+    cy.get("[data-testid=home-page-link]").as("homePageLink");
+    cy.get("[data-testid=lists-page-link]").as("listsLink");
+    cy.get("[data-testid=shopping-cart-link]").as("cartLink");
+
+    cy.get("@listsLink").click();
+    cy.url().should((url) => {
+      expect(url).to.eq(Cypress.config().baseUrl + "/favourites");
+    });
+
+    cy.get("@cartLink").click();
+    cy.url().should((url) => {
+      expect(url).to.eq(Cypress.config().baseUrl + "/shoppingcart");
+    });
+
+    cy.get("@homePageLink").click();
+    cy.url().should((url) => {
+      expect(url).to.eq(Cypress.config().baseUrl + "/");
+    });
+  });
+});
