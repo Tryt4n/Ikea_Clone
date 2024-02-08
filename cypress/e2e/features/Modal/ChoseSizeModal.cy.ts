bfbfdcc4a9824d37782dbe0a38d2fc60a4eb7dc1
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-08 13:03:39 +0100

    Added tests.

---
+++ cypress/e2e/features/Modal/ChoseSizeModal.cy.ts
@@ -0,0 +1,48 @@
+describe("Chose size modal", () => {
+  beforeEach(() => {
+    cy.visit("/products/korken/sloik-z-pokrywka/szklo-bezbarwne/50213546");
+    cy.wait("@getProductPage");
+    cy.wait(300); // eslint-disable-line cypress/no-unnecessary-waiting
+  });
+
+  it("should open chose size modal", () => {
+    cy.get("[data-testid=modal]").as("modal");
+    cy.get("[data-testid=modal-control-btn-size]").as("modalControlBtnSize");
+
+    cy.get("@modalControlBtnSize").click();
+    cy.get("@modal")
+      .should("be.visible")
+      .and("have.class", "show")
+      .and("have.class", "side-modal")
+      .and("have.attr", "open");
+
+    cy.get("@modal").within(() => {
+      cy.get("h2").should("contain.text", "Wybierz rozmiar");
+
+      cy.fixture("productPage.json").then((fixture) => {
+        cy.get("[data-testid=modal-product-size]").as("productSizes");
+
+        cy.get("@productSizes").should(
+          "have.length",
+          Object.keys(fixture.relatedProducts.sizes).length,
+        );
+
+        cy.get("@productSizes").each(($child) => {
+          if ($child.text().includes(fixture.size)) {
+            cy.wrap($child).should("have.prop", "tagName").and("eq", "DIV");
+          } else {
+            cy.wrap($child).should("have.prop", "tagName").and("eq", "A");
+          }
+        });
+
+        cy.get("@productSizes").first().click();
+        cy.url().should((url) => {
+          expect(url).to.eq(
+            Cypress.config().baseUrl +
+              `/products/korken/sloik-z-pokrywka/szklo-bezbarwne/${fixture.relatedProducts.sizes[Object.keys(fixture.relatedProducts.sizes)[0]]}`,
+          );
+        });
+      });
+    });
+  });
+});
---
+++ cypress/e2e/features/Modal/ImagePreviewModal.cy.ts
@@ -0,0 +1,96 @@
+describe("Image preview modal", () => {
+  beforeEach(() => {
+    cy.visit("/products/korken/sloik-z-pokrywka/szklo-bezbarwne/50213546");
+    cy.wait("@getProductPage");
+    cy.wait(300); // eslint-disable-line cypress/no-unnecessary-waiting
+
+    cy.get("[data-testid=modal]").as("modal");
+    cy.get("[data-testid=product-image-gallery-item]").as(
+      "productImageGalleryItems",
+    );
+  });
+
+  it("should open image preview modal", () => {
+    // Click second image
+    const secondImageIndex = 1;
+    cy.get("@productImageGalleryItems").eq(secondImageIndex).click();
+    cy.get("@modal")
+      .should("be.visible")
+      .and("have.class", "show")
+      .and("have.class", "image-modal")
+      .and("have.attr", "open");
+
+    cy.get("@modal").within(() => {
+      cy.get("img").as("modalImages");
+      cy.get("video").as("modalVideos");
+      cy.get('div[role="button"][aria-label="Previous slide"]').as(
+        "prevButton",
+      );
+      cy.get('div[role="button"][aria-label="Next slide"]').as("nextButton");
+    });
+
+    cy.fixture("productPage.json").then((fixture) => {
+      cy.get("@modalVideos").then(($videos) => {
+        const videoCount = $videos.length;
+
+        cy.get("@modalImages").should(
+          "have.length",
+          Object.keys(fixture.images).length - videoCount,
+        );
+      });
+
+      cy.get("@modalImages").each(($image, index) => {
+        if (!fixture.images.video) {
+          cy.wrap($image)
+            .should("have.attr", "src")
+            .and("contain", fixture.images[Object.keys(fixture.images)[index]]);
+        }
+
+        if (index === secondImageIndex) {
+          cy.wrap($image).should("be.visible");
+        } else {
+          cy.wrap($image).should("not.be.visible");
+        }
+      });
+
+      // Click the previous button
+      cy.get("@prevButton").click();
+      cy.get("@modalImages").each(($image, index) => {
+        if (index === secondImageIndex - 1) {
+          cy.wrap($image).should("be.visible");
+        } else {
+          cy.wrap($image).should("not.be.visible");
+        }
+      });
+
+      // Click the next button
+      cy.get("@nextButton").click();
+      cy.get("@modalImages").each(($child, index) => {
+        if (index === secondImageIndex) {
+          cy.wrap($child).should("be.visible");
+        } else {
+          cy.wrap($child).should("not.be.visible");
+        }
+      });
+    });
+  });
+
+  it("should open image preview modal with video", () => {
+    cy.fixture("productPage.json").then((fixture) => {
+      const videoIndex = Object.keys(fixture.images).findIndex(
+        (key) => key === "video",
+      );
+
+      cy.get("@productImageGalleryItems").eq(videoIndex).click("bottomLeft"); // Make sure not to click the play button
+
+      cy.get("@modal").within(() => {
+        cy.get("video").as("modalVideo");
+      });
+
+      cy.get("@modalVideo").should("be.visible").and("have.attr", "autoplay");
+    });
+
+    cy.get("body").type("{esc}");
+    cy.get("@modal").should("not.be.visible");
+  });
+});
